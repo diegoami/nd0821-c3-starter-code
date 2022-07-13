bfd1056839859503edfbc2c4487b1a90e02e012c
Diego Amicabile <diego.amicabile@gmail.com>
2022-07-13 22:58:34 +0200

    Additional Pytest

--- starter/starter/test/model_test.py
+++ starter/starter/test/model_test.py
@@ -1,6 +1,7 @@
 import unittest
+
 import pytest
-from ml.model import train_model, compute_model_metrics
+from ml.model import train_model, compute_model_metrics, inference
 import numpy as np
 
 
@@ -11,6 +12,22 @@ class ModelTestCase(unittest.TestCase):
         y = np.array([0, 0, 0, 1])
         model = train_model(X, y)
         self.assertIsNotNone(model)
+
+    def test_compute_model_metrics(self):
+        y = np.array([0,0,0,1])
+        preds = np.array([0,0,0,1])
+        precision, recall, fbeta = compute_model_metrics(y, preds)
+        self.assertEqual(precision, 1)
+        self.assertEqual(recall, 1)
+        self.assertEqual(fbeta, 1)
+
+    def test_inference(self):
+        class model_mock:
+            def predict(X):
+                return np.array([1, 1, 1])
+        X = np.array([0, 0, 0])
+        y = inference(model_mock, X)
+        self.assertIsNotNone(y)
 
 if __name__ == '__main__':
     unittest.main()
