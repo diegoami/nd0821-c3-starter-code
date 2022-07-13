2147cbaf636e435d418c5e4c5b14df7bb29ec0d0
Diego Amicabile <diego.amicabile@gmail.com>
2022-07-13 23:40:47 +0200

    Train model taking account

--- starter/starter/evaluate_model.py
+++ starter/starter/evaluate_model.py
@@ -10,11 +10,23 @@ import joblib
 
 
 from ml.data import process_data
-from ml.model import train_model, compute_model_metrics, inference
+from ml.model import train_model, inference, compute_model_metrics
+
+
+def perform_data(df, feature, model, categorical_features):
+    print("==== FEATURE : {feature}")
+    for cls in df[feature].unique():
+        print("==CLS  : {cls}")
+        df_temp = df[df[feature] == cls]
+        X, y, _, _ = process_data(df_temp, categorical_features=categorical_features, label="salary", training = True)
+        preds = inference(model, X)
+        compute_model_metrics(y, preds)
+    print()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Train a model",
+        description="Evalaute a model",
         fromfile_prefix_chars="@",
     )
 
@@ -36,9 +48,14 @@ if __name__ == "__main__":
     # Add code to load in the data.
     args = parser.parse_args()
     data = pd.read_csv(args.data_file_name)
-    # Optional enhancement, use K-fold cross validation instead of a train-test split.
-    train, test = train_test_split(data, test_size=0.20, shuffle=True, stratify=data["salary"])
 
+    # Add code to load in the data.
+
+
+    # Add code to load in the data.
+    model = joblib.load(args.model_file_name)
+
+    # Train and save a model.
     cat_features = [
         "workclass",
         "education",
@@ -49,23 +66,8 @@ if __name__ == "__main__":
         "sex",
         "native-country",
     ]
-    X_train, y_train, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
-    )
 
-    # Proces the test data with the process_data function.
-    X_test, y_test, _, _ = process_data(
-        test, categorical_features=cat_features, label="salary", encoder = encoder, lb = lb, training = False
-    )
-
-    model = train_model(X_train, y_train)
-    filename = args.model_file_name
-
-    joblib.dump(model, filename)
-    preds = inference(model, X_test)
-    precision, recall, fbeta = compute_model_metrics(y_test, preds)
-    # Train and save a model.
-    print(f"precision: {precision}, recall: {recall}, fbeta: {fbeta}")
-
+    for feature in cat_features:
+        perform_data(data, feature, model, cat_features)
 
 
