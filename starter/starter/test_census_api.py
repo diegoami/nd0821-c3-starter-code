12e2b8e37901224fd35346847eef0b7126d82caf
Diego Amicabile <diego.amicabile@gmail.com>
2022-07-31 21:58:32 +0200

    Predictions work mit fastapi

--- starter/starter/test_census_api.py
+++ starter/starter/test_census_api.py
@@ -6,24 +6,29 @@ from census_api import app
 client = TestClient(app)
 
 dummy_census_entry = {
-    'age': 20,
-    'workclass' : 'State-gov',
-    'fnlgt': 3000,
-    'education': 'Doctorate',
-    'education_num': 20,
-    'marital_status': 'Never-married',
-    'occupation': 'Sales',
-    'relationship': 'Not-in-family',
-    'race': 'White',
-    'sex': 'Male',
-    'capital_gain': 10000,
-    'capital_loss': 0,
-    'hours_per_week': 40,
-    'native_country': 'United-States'
+    "age": 20,
+    "workclass": "State-gov",
+    "fnlgt": 3000,
+    "education": "Doctorate",
+    "education-num": 20,
+    "marital-status": "Never-married",
+    "occupation": "Sales",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 10000,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
 }
 
 
 
-def test_post():
+def test_print_out():
+    r = client.post("/print_out", json=dummy_census_entry)
+    print(r.json())
+
+
+def test_predict():
     r = client.post("/predict", json=dummy_census_entry)
     print(r.json())
